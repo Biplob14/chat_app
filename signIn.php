6f75d51caf8364b33d74c47d9a62496f81e3de4e
biplob14 <biploburrahaman@gmail.com>
2020-06-30 01:14:41 +0600

    front end major part

---
+++ signIn.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login to your account</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/signIn.css">  <!--custom css-->
+
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <!-- ajax link -->
+    <script src="https://ajax.googleapis.com/ajax/libs/d3js/5.15.1/d3.min.js"></script> 
+</head>
+<body>
+    <div class="signIn-form">
+        <form action="" method="post">
+            <div class="form-header">
+                <h2>Sign In</h2>
+                <p>Login to MyChat</p>
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="email" name="email" class="form-control" placeholder="Provide your email" autocomplete="off" required>
+            </div>
+            <div class="form-group">
+                <label for="">Password</label>
+                <input type="password" name="pass" class="form-control" placeholder="Provide your password" autocomplete="off" required>
+            </div>
+            <div class="small">Forgot Password? <a href="forgot_pass.php"> Click Here</a></div> <br>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block btn-lg" name="sign_in">Sign In</button>
+            </div>
+            <!-- <?php include "signIn_user.php" ?> -->
+        </form>
+        <div class="text-center small" style="color: #67428B;">Don't have an account? <a href="signUp.php">Create One</a></div>
+    </div>
+</body>
+</html>
